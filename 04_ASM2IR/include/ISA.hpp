48c93b7076d75cf48bf78c73616705af8b97dfb0
Vlad Lazar <lazar_2004@list.ru>
2025-11-12 23:48:38 +0300

    New commands for ISA

--- 04_ASM2IR/include/ISA.hpp
+++ 04_ASM2IR/include/ISA.hpp
@@ -114,9 +114,9 @@
 //==============================================================================
 
 //============================IRGenExecute======================================
-#define GEP2_64(arg) builder.CreateConstGEP2_64(regFileType, regFile, 0, arg)
-// #define LOAD_PTR(arg) Builder.CreateLoad(int32PtrType, GEP2_64(arg))
-#define LOAD_REG(arg) builder.CreateLoad(int64Type, GEP2_64(arg))
+#define GEP2_64(arg) builder.CreateConstGEP2_64(regFileType, reg_file, 0, arg)
+#define STORE(from_, to_) builder.CreateStore(from_, to_)
+#define LOAD_REG(arg) builder.CreateLoad(int64Ty, GEP2_64(arg))
 #define GEN_IMM(arg) builder.getInt64(arg)
 
 // ISA(Opcode_, Name_, SkipArgs_, ReadArgs_, WriteArgs_, Execute_,
@@ -128,17 +128,28 @@ ISA(
     0x2, ADD, SKIP_3ARGS, READ_3REGS, WRITE_3REGS,
     { cpu->reg_file[rd] = cpu->reg_file[r1] + cpu->reg_file[r2imm]; },
     {
-      builder.CreateStore(builder.CreateAdd(LOAD_REG(I.r1), LOAD_REG(I.r2imm)),
-                          GEP2_64(I.rd));
+      STORE(builder.CreateAdd(LOAD_REG(I.r1), LOAD_REG(I.r2imm)),
+            GEP2_64(I.rd));
     })
 
 ISA(
-    0x3, MOVi, SKIP_2ARGS, READ_REG_IMM, WRITE_REG_IMM,
-    { cpu->reg_file[rd] = r2imm; },
-    { builder.CreateStore(GEN_IMM(I.r2imm), GEP2_64(I.rd)); })
+    0x3, ADDi, SKIP_3ARGS, READ_2REGS_IMM, WRITE_2REGS_IMM,
+    { cpu->reg_file[rd] = cpu->reg_file[r1] + r2imm; },
+    {
+      STORE(builder.CreateAdd(LOAD_REG(I.r1), GEN_IMM(I.r2imm)), GEP2_64(I.rd));
+    })
 
 ISA(
-    0x4, BR_COND, SKIP_3ARGS, READ_REG_3LABEL, WRITE_REG_3LABEL,
+    0x4, MOV, SKIP_2ARGS, READ_2REGS, WRITE_2REGS,
+    { cpu->reg_file[rd] = cpu->reg_file[r1]; },
+    { STORE(LOAD_REG(I.r1), GEP2_64(I.rd)); })
+
+ISA(
+    0x5, MOVi, SKIP_2ARGS, READ_REG_IMM, WRITE_REG_IMM,
+    { cpu->reg_file[rd] = r2imm; }, { STORE(GEN_IMM(I.r2imm), GEP2_64(I.rd)); })
+
+ISA(
+    0x6, BR_COND, SKIP_3ARGS, READ_REG_3LABEL, WRITE_REG_3LABEL,
     {
       if (cpu->reg_file[rd]) {
         cpu->next_pc = r2imm;
@@ -150,14 +161,66 @@ ISA(
       builder.CreateCondBr(
           builder.CreateTrunc(LOAD_REG(I.rd), builder.getInt1Ty()),
           BBMap[I.r2imm], BBMap[I.r3imm]);
-      builder.SetInsertPoint(BBMap[++PC]);
+      builder.SetInsertPoint(BBMap[++pc]);
       continue;
     })
 
 ISA(
-    0x5, BRANCH, SKIP_1ARG, READ_2LABEL, WRITE_2LABEL, { cpu->next_pc = r2imm; },
+    0x7, BRANCH, SKIP_1ARG, READ_2LABEL, WRITE_2LABEL,
+    { cpu->next_pc = r2imm; },
     {
       builder.CreateBr(BBMap[I.r2imm]);
-      builder.SetInsertPoint(BBMap[++PC]);
+      builder.SetInsertPoint(BBMap[++pc]);
       continue;
+    })
+
+ISA(
+    0x8, CMP_EQ, SKIP_3ARGS, READ_2REGS_IMM, WRITE_2REGS_IMM,
+    { cpu->reg_file[rd] = (cpu->reg_file[r1] == r2imm); },
+    {
+      STORE(builder.CreateZExt(
+                builder.CreateICmpEQ(LOAD_REG(I.r1), GEN_IMM(I.r2imm)),
+                builder.getInt64Ty()),
+            GEP2_64(I.rd));
+    })
+
+ISA(
+    0x9, ANDi, SKIP_3ARGS, READ_2REGS_IMM, WRITE_2REGS_IMM,
+    { cpu->reg_file[rd] = cpu->reg_file[r1] & r2imm; },
+    {
+      STORE(builder.CreateAnd({LOAD_REG(I.r1), GEN_IMM(I.r2imm)}),
+            GEP2_64(I.rd));
+    })
+
+ISA(
+    0xa, SIM_RAND, SKIP_1ARG, READ_REG(rd), WRITE_REG(rd),
+    { cpu->reg_file[rd] = simRand(); },
+    {
+      STORE(
+          builder.CreateSExt(builder.CreateCall(simRand), builder.getInt64Ty()),
+          GEP2_64(I.rd));
+    })
+
+ISA(
+    0xb, SCREEN_FLUSH, {}, {}, {}, { simFlush(); },
+    { builder.CreateCall(simFlush); })
+
+ISA(
+    0xc, SCREEN_PUT_PIXEL, SKIP_3ARGS, READ_3REGS, WRITE_3REGS,
+    {
+      simPutPixel(cpu->reg_file[rd], cpu->reg_file[r1], cpu->reg_file[r2imm]);
+    },
+    {
+      builder.CreateCall(
+          simPutPixel,
+          {builder.CreateTrunc(LOAD_REG(I.rd), builder.getInt32Ty()),
+           builder.CreateTrunc(LOAD_REG(I.r1), builder.getInt32Ty()),
+           builder.CreateTrunc(LOAD_REG(I.r2imm), builder.getInt32Ty())});
+    })
+
+ISA(
+    0xd, SHL, SKIP_3ARGS, READ_2REGS_IMM, WRITE_2REGS_IMM,
+    { cpu->reg_file[rd] = cpu->reg_file[r1] << r2imm; },
+    {
+      STORE(builder.CreateShl(LOAD_REG(I.r1), GEN_IMM(I.r2imm)), GEP2_64(I.rd));
     })
